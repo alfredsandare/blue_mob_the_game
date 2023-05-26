9d23794b049226c57bab85af9dbafd6b813dba62
alfredsandare <alfred.s.sandare04@gmail.com>
2023-05-26 15:50:50 +0200

    mer stuff

--- game.py
+++ game.py
@@ -23,14 +23,15 @@ class Frame:
             'bullet': pygame.image.load(path+'graphics\\bullet.png'),
             'bullet2': pygame.image.load(path+'graphics\\bullet2.png'),
             'enemy1': pygame.image.load(path+'graphics\\enemy1.png'),
-            'game_over': pygame.image.load(path+'graphics\\game_over.png')
+            'enemy2': pygame.image.load(path+'graphics\\enemy2.png'),
+            'game_over': pygame.image.load(path+'graphics\\game_over.png'),
+            'purchase_bar': pygame.image.load(path+'graphics\\purchase_bar.png')
         }
 
 frame = Frame()
 
 class Coin:
     def __init__(self, pos):
-        #self.pos = [random.randint(0, 960), random.randint(0, 540)]
         self.pos = pos
 
     def check_collision(self):
@@ -48,11 +49,13 @@ class Bullet:
         self.pos[0] += 10 * math.cos(self.direction)
 
     def check_collision(self):
-        if self.is_friendly:  # check collisions with enemy
-            for entity in map_data:
-                if entities_overlap(self.pos, (20, 20), (entity[0], entity[1]), (entity[2], entity[3])):
-                    return True
-        else:
+        # check collision with walls
+        for entity in map_data:
+            if entities_overlap(self.pos, (20, 20), (entity[0], entity[1]), (entity[2], entity[3])):
+                return True
+
+        # for enemy bullets, check collision with player
+        if not self.is_friendly:
             if entities_overlap(self.pos, (20, 20), player.pos, (40, 40)):
                 if not player.god_mode:
                     player.hp -= 1
@@ -60,7 +63,7 @@ class Bullet:
                 return True
 
         #if the bullet is not in frame, delete it
-        if not entities_overlap(self.pos, (20, 20), (0, 0,), frame.size):
+        if not entities_overlap(self.pos, (20, 20), (0, 0), frame.size):
             return True
         return False
 
@@ -74,7 +77,7 @@ class Enemy:
         self.size = 40
         self.vel = 1.5
         self.hp = 3
-        self.coin_drop_chance = 1
+        self.coin_drop_chance = 0.5
         self.attack_type = attack_type  # 'hit' or 'shoot'
         self.frames_since_last_attack = 0
 
@@ -122,28 +125,23 @@ class Enemy:
                 if self.walk_queue[2] == 0:
                     self.walk_queue = None
 
+        # move the enemy if there are no walls in the way
+        if to_move_x < 0 and (all([not (entity[0] - 40 < self.pos[0] < entity[2] + entity[0] + to_move and
+            entity[1] - 40 < self.pos[1] < entity[3] + entity[1]) for entity in map_data]) or self.pos[0] > frame.size[0] - 40):
+            self.pos[0] += to_move_x
+    
+        if to_move_x > 0 and (all([not (entity[0] - 40 - to_move < self.pos[0] < entity[2] + entity[0] and
+            entity[1] - 40 < self.pos[1] < entity[3] + entity[1]) for entity in map_data]) or self.pos[0] < 0):
+            self.pos[0] += to_move_x
 
+        if to_move_y < 0 and (all([not (entity[1] - 40 < self.pos[1] < entity[3] + entity[1] + to_move and
+            entity[0] - 40 < self.pos[0] < entity[2] + entity[0]) for entity in map_data]) or self.pos[1] > frame.size[1] -40):
+            self.pos[1] += to_move_y
 
-        '''
-        if to_move_x < 0 and not all([not (entity[0] - self.size < self.pos[0] < entity[2] + entity[0] + to_move_x and
-            entity[1] - self.size < self.pos[1] < entity[3] + entity[1]) for entity in map_data]):
-            to_move_x = 0
-        
-        if to_move_x > 0 and not all([not (entity[0] - self.size - to_move_x < self.pos[0] < entity[2] + entity[0] and
-            entity[1] - self.size < self.pos[1] < entity[3] + entity[1]) for entity in map_data]):
-            to_move_x = 0
-
-        if to_move_y < 0 and not all([not (entity[1] - self.size < self.pos[1] < entity[3] + entity[1] + to_move_y and
-            entity[0] - self.size < self.pos[0] < entity[2] + entity[0]) for entity in map_data]):
-            to_move_y = 0
-
-        if to_move_y > 0 and not all([not (entity[1] - self.size - to_move_y < self.pos[1] < entity[3] + entity[1] and
-            entity[0] - self.size < self.pos[0] < entity[2] + entity[0]) for entity in map_data]):
-            to_move_y = 0
-        '''
-        
-        self.pos[0] += to_move_x
-        self.pos[1] += to_move_y
+        if to_move_y > 0 and (all([not (entity[1] - 40 - to_move < self.pos[1] < entity[3] + entity[1] and
+            entity[0] - 40 < self.pos[0] < entity[2] + entity[0]) for entity in map_data]) or self.pos[1] < 0):
+            self.pos[1] += to_move_y
+    
 
     def check_collision(self):
         for i, bullet in enumerate(bullets):
@@ -167,10 +165,18 @@ class Enemy:
                 bullets.append(Bullet(direction_to_player, copy(self.pos), is_friendly=False))
                 self.frames_since_last_attack = 0
 
+    def blit_id(self, id):
+        # for debug. Puts the enemy id number on the enemy
+        font = pygame.font.SysFont('arial', 32)
+        text = str(id)
+        rendered_text = font.render(text, True, (255, 255, 255))
+        #size = font.size(text)
+        frame.screen.blit(rendered_text, self.pos)
+
 
 class Player:
     def __init__(self):
-        self.pos = [600, 300]
+        self.pos = [460, 250]
         self.size = 40
         self.vel = 3
         self.hp = 20
@@ -224,6 +230,78 @@ class CoinCount:
 coin_count = CoinCount()
 
 
+class WaveCount:
+    def __init__(self):
+        self.pos = (950, 10) #top-right corner is the anchor point
+
+    def update(self):
+        font = pygame.font.SysFont('arial', 32)
+        text = f'{current_wave+1}{get_number_suffix(current_wave+1)} wave'
+        rendered_text = font.render(text, True, (255, 255, 255))
+        size = font.size(text)
+        frame.screen.blit(rendered_text, (self.pos[0]-size[0], self.pos[1]))
+
+wave_count = WaveCount()
+
+class Turret:
+    def __init__(self, pos=[0, 0]):
+        self.pos = pos
+        self.timer = 600  # how many frames left until turret is destroyed
+        self.frames_since_last_attack = 0
+        self.shoot_frequency = 30
+        self.closest_enemy = -1
+        self.fg_image_angle = 45
+
+        self.bg_image = pygame.image.load(path+'graphics\\turret_bg.png')
+        self.fg_image = pygame.image.load(path+'graphics\\turret_fg.png')
+
+    def update(self, blit_target_id=False):
+        frame.screen.blit(self.bg_image, self.pos)
+        rotated_fg_image = pygame.transform.rotate(self.fg_image, self.fg_image_angle+180)
+        rect = rotated_fg_image.get_rect(center=self.fg_image.get_rect(topleft=(self.pos[0]+7, self.pos[1]+7)).center)
+        frame.screen.blit(rotated_fg_image, rect) #(self.pos[0]+7, self.pos[1]+7))
+
+        pygame.draw.rect(frame.screen, (0, 0, 0), pygame.Rect(self.pos[0]+2, self.pos[1]+44, 36, 6))
+        pygame.draw.rect(frame.screen, (237, 28, 36), pygame.Rect(self.pos[0]+3, self.pos[1]+45, 34 * self.timer/600, 4))
+
+        if len(enemies) > 0:
+            #find the closest enemy
+            smallest_distance = 999
+            for i, enemy in enumerate(enemies):
+                distance_to_enemy = math.sqrt((enemy.pos[0]-self.pos[0])**2 + (enemy.pos[1]-self.pos[1])**2)
+                if distance_to_enemy < smallest_distance and 0 < enemy.pos[0] < frame.size[0] and 0 < enemy.pos[1] < frame.size[1]:
+                    smallest_distance = distance_to_enemy
+                    self.closest_enemy = i
+
+            #calculate the angle
+            target_pos = enemies[self.closest_enemy].pos
+            pos = [self.pos[0]+20, self.pos[1]+20]
+            shoot_direction = math.atan((pos[1] - (target_pos[1]+20))/((target_pos[0]+20) - pos[0]))
+            if target_pos[0]+20 < pos[0]:
+                shoot_direction += math.pi
+            self.fg_image_angle = math.degrees(shoot_direction)
+
+            #shoot the closest enemy
+            if self.frames_since_last_attack >= self.shoot_frequency and len(enemies) > 0:
+                bullets.append(Bullet(shoot_direction, pos))
+                self.frames_since_last_attack = 0
+            else:
+                self.frames_since_last_attack += 1
+
+
+        if blit_target_id:
+            # for debug. Puts the enemy id number on the enemy
+            font = pygame.font.SysFont('arial', 32)
+            text = str(self.closest_enemy)
+            rendered_text = font.render(text, True, (255, 255, 255))
+            #size = font.size(text)
+            frame.screen.blit(rendered_text, self.pos)
+
+        self.timer -= 1
+
+
+turrets = []
+
 
 def entities_overlap(coords1, size1, coords2, size2):
     ''' There are two rectangles, 1 and 2.
@@ -241,11 +319,13 @@ def shoot():
         direction += math.pi
     pos = [player.pos[0]+20, player.pos[1]+20]  # start at the player's position
     bullets.append(Bullet(direction, pos))
-    #print(direction)
 
 with open(path+'data\\map_data.json') as file:
     map_data = json.load(file)
 
+def get_number_suffix(n):
+    ''' returns the english suffix from any given positive integer '''
+    return ['st', 'nd', 'rd'][n-1] if 0 < n < 4 else 'th'
 
 coins = []
 bullets = []
@@ -259,12 +339,8 @@ current_wave = 0
 enemies_spawned_this_wave = 0
 
 
-enemies.append(Enemy(pos=[200, 200], behaviour='long-distance', attack_type='shoot'))
-
-
 
 while 1:
-
 
     timeStamp = time.perf_counter()
 
@@ -276,6 +352,16 @@ while 1:
                 shoot()
             elif event.unicode == 'g':
                 player.god_mode = not player.god_mode
+            elif event.unicode == '1':
+                if player.coins >= 10:
+                    player.coins -= 10
+                    player.hp += 10
+                    if player.hp > 20:
+                        player.hp = 20
+
+            elif event.unicode == '2':
+                turrets.append(Turret(pos=copy(player.pos)))
+
         elif event.type == 769:
             current_keydowns.remove(event.unicode)
         elif event.type == 1024:
@@ -287,6 +373,7 @@ while 1:
     if sum([int(letter in current_keydowns) for letter in ['w', 'a', 's', 'd']]) > 1:  # if moving diagonally
         to_move *= 1/math.sqrt(2)
 
+    # only move the player if there are not any walls in the way
     if 'a' in current_keydowns and all([not (entity[0] - player.size < player.pos[0] < entity[2] + entity[0] + to_move and
         entity[1] - player.size < player.pos[1] < entity[3] + entity[1]) for entity in map_data]):
         player.pos[0] -= to_move
@@ -339,21 +426,22 @@ while 1:
                 enemies_deleted += 1
 
     frame.screen.blit(frame.images['player'], player.pos)
+    frame.screen.blit(frame.images['purchase_bar'], (360, 460))
+
+    #ge spelaren en näsa
     #pygame.draw.line(frame.screen, (0, 0, 0), mouse_pos, (player.pos[0]+20, player.pos[1]+20))
 
     blood_flash.update()
     hp_bar.update()
     coin_count.update()
+    wave_count.update()
 
-    '''
-    if random.random() < intensity_factor:
-        pos = [random.randint(0, 960), random.randint(0, 540)]
-        #behaviour = 'charge' if random.random() < 0.6 else 'clueless'
-        behaviour = 'long-distance'
-        enemies.append(Enemy(pos=pos, behaviour=behaviour, attack_type='shoot'))
-    '''
+    turrets_deleted = 0
+    for i, turret in enumerate(turrets):
+        turret.update()
+        if turret.timer == 0:
+            del turrets[i - turrets_deleted]
 
-    print(enemies_spawned_this_wave, current_wave * 3 +5)
     if enemies_spawned_this_wave < current_wave * 3 + 5 and random.random() < intensity_factor:
         spawn_side = random.randint(0, 3)
         if spawn_side == 0:  # right side
@@ -365,18 +453,18 @@ while 1:
         else:  # bottom side
             pos = [random.randint(-100, 1060), random.randint(540, 640)]
 
-        enemy_sets = [['long-distance', 'shoot'], ['charge', 'hit']]
+        enemy_sets = [['long-distance', 'shoot', 'enemy1'], ['charge', 'hit', 'enemy2']]
         if random.random() < 0.4 * math.e**(-0.1*current_wave) + 0.3:
             enemy_type = 1
         else:
             enemy_type = 0
-        enemies.append(Enemy(pos=pos, behaviour=enemy_sets[enemy_type][0], attack_type=enemy_sets[enemy_type][1]))
+        enemies.append(Enemy(pos=pos, behaviour=enemy_sets[enemy_type][0], attack_type=enemy_sets[enemy_type][1], texture=enemy_sets[enemy_type][2]))
         enemies_spawned_this_wave += 1
 
     if enemies_spawned_this_wave == current_wave * 3 + 5 and len(enemies) == 0: #reset wave
-        print('resetinn wave')
         current_wave += 1
         enemies_spawned_this_wave = 0
+        intensity_factor *= 1.1
 
 
     if player.hp < 1:
